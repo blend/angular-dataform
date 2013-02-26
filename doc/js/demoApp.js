b6c20f1d62e189e896e845cf54973c8a11a53b34
Quinn Slack <qslack@qslack.com>
2013-02-26 03:27:39 -0800

    Documentation page with tags example

---
+++ doc/js/demoApp.js
@@ -0,0 +1,25 @@
+
+/**
+ * demoApp - 1.0.0rc2
+ */
+
+angular.module('demoApp', ['dataform'], function($locationProvider) {
+  $locationProvider.hashPrefix('');
+  // Make code pretty
+  window.prettyPrint && prettyPrint();
+}).directive('scrollto', [function(){
+  return function(scope, elm, attrs) {
+    elm.bind('click', function(e){
+      e.preventDefault();
+      if (attrs.href) {
+	attrs.scrollto = attrs.href;
+      }
+      var top = $(attrs.scrollto).offset().top;
+      $('body,html').animate({ scrollTop: top }, 800);
+    });
+  };
+}]);
+
+function TagCtrl($scope) {
+  $scope.tags = ['Alice', 'Bob', 'Carol'];
+}
