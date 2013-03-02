82deb69658fef230005fe6cd78519b071e9c6ee4
Quinn Slack <qslack@qslack.com>
2013-03-01 18:01:07 -0800

    Remove console.log

--- modules/directives/autocomplete/autocomplete.js
+++ modules/directives/autocomplete/autocomplete.js
@@ -34,7 +34,6 @@ angular.module('dataform.directives').directive('dfAutocompleteDatalist', ['$doc
           // Allow <input ... df-autocomplete-submit-on-select> option that instructs this
           // directive to submit its parent form when the user makes a selection.
           if (attrs.hasOwnProperty('dfAutocompleteSubmitOnSelect')) {
-            console.log('SELECT');
             if (elem.parent().is('form')) {
               elem.parent().submit();
             }
